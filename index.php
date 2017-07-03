d4a39854e9202e3f49241400ba9ca55574781150
bruce <bruce@bruce.net.cn>
2017-07-03 18:53:35 +0800

    fix

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 define('ROOT', __DIR__);
+define('APPLICATION_PATH', dirname(__FILE__));
 require_once(ROOT . '/vendor/autoload.php');
-use BCache\App\Application\init;
-init::start()->run();
+$application = new Yaf_Application( APPLICATION_PATH . "/conf/application.ini" );
+
+$application->bootstrap()->run();
+/* use BCache\App\Application\init; */
+/* init::start()->run(); */
--- src/BCache/Libs/Controller/ControllerBase.php
+++ src/BCache/Libs/Controller/ControllerBase.php
@@ -2,12 +2,12 @@
 namespace BCache\Libs\Controller;
 use Pux\Controller;
 use BCache\App\Smarty\Smarty;
-class ControllerBase extends Controller
+class ControllerBase extends Yaf_Controller_Abstract
 {
-	protected $smerty;
-	public function __construct()
-	{
-		$sm = new Smarty();
-		$this->smarty = $sm->get();
-	}
+	/* protected $smerty; */
+	/* public function __construct() */
+	/* { */
+	/* 	$sm = new Smarty(); */
+	/* 	$this->smarty = $sm->get(); */
+	/* } */
 }
